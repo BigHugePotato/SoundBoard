e9195cf708d572e91147a8636d3cd1171094b094
adrianb09 <adrianb09.kodehode@gmail.com>
2024-01-10 14:52:43 +0100

    added sounds og buttons. Funker ikke enda

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+// document.addEventListener("DOMContentLoaded", (event) => {
 const sounds = [
-    { fileName : "dry-fart.mp3"},
-    {fileName : "eD"},
-    {fileName : "your-phone-linging.mp3"}
-]
+  { fileName: "dry-fart.mp3" },
+  { fileName: "eD" },
+  { fileName: "your-phone-linging.mp3" },
+];
 
+const soundBoard = document.getElementById("soundboard");
+
+const createSoundBoard = (audioscr) => {
+  const buttonEl = document.createElement("button");
+  buttonEl.textContent = audioscr.fileName;
+
+  const audioEl = document.createElement("audio");
+
+  audioEl.src = `${SoundEffects}/${audioscr.fileName}`
+
+  audioscr.audioEl = audioEl
+};
+
+sounds.forEach((soundObj) => {
+  soundBoard.append(createSoundBoard(soundObj));
+});
+
+createSoundBoard();
 
