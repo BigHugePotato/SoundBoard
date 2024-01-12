e113d7ab18995dd7da603e9c20f36d1d65d048a6
adrianb09 <adrianb09.kodehode@gmail.com>
2024-01-12 13:06:37 +0100

    flexwrap

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const sounds = [
   { fileName: "dry-fart.mp3", name: "Fart Dry" },
+  { fileName: "perfect-fart.mp3", name: "Fart Wet" },
   { fileName: "eD.mp3", name: "Emotional Damage" },
   { fileName: "your-phone-linging.mp3", name: "Phone Linging" },
   { fileName: "ah-shit-here-we-go-again.mp3", name: "Again" },
@@ -11,7 +12,6 @@ const sounds = [
   { fileName: "clash-royale-hog-rider.mp3", name: "Hog Rider" },
   { fileName: "dog-doing.mp3", name: "Dog Doing" },
   { fileName: "gay-echo.mp3", name: "Gay Echo" },
-  { fileName: "perfect-fart.mp3", name: "Fart Wet" },
   { fileName: "toothless-dancing.mp3", name: "Dragon Dance" },
   { fileName: "vine-boom.mp3", name: "Vine Boom" },
   { fileName: "yeah-boiii.mp3", name: "Yeah Boi" },
@@ -22,28 +22,27 @@ const soundBoard = document.getElementById("soundboard");
 const createSoundBoard = (audioscr) => {
   const buttonEl = document.createElement("button");
   buttonEl.textContent = audioscr.name;
-//   buttonEl.addEventListener("click", () => audioEl.play());
 
   const audioEl = document.createElement("audio");
   audioEl.src = `SoundEffects/${audioscr.fileName}`;
-    audioscr.audioEl = audioEl;
-    audioscr.isPlaying = false;
+  audioscr.audioEl = audioEl;
+  audioscr.isPlaying = false;
 
   buttonEl.addEventListener("click", () => {
     if (audioscr.isPlaying) {
       audioEl.pause();
       audioEl.currentTime = 0;
-      audioscr.isPlaying = false
+      audioscr.isPlaying = false;
       buttonEl.classList.remove("rainbow-animation", "btnDown");
     } else {
       audioEl.play();
-      audioscr.isPlaying = true
+      audioscr.isPlaying = true;
       buttonEl.classList.add("rainbow-animation", "btnDown");
     }
   });
 
   audioEl.addEventListener("ended", () => {
-    audioscr.isPlaying = false
+    audioscr.isPlaying = false;
     buttonEl.classList.remove("rainbow-animation", "btnDown");
   });
   return buttonEl;
